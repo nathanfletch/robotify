f8ec65d6cc63270d469ed298d9e414645c917bf1
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-20 08:32:27 -0700

    Add initial files with basic HTML input/output fields

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//ui logic
+// $(document).ready(function () {
+//   $("form").submit(function (event) {
+//     event.preventDefault();
+//     const text = $("#input1").val();
+//     $("#display-text").text(text);
+//   });
+// });
