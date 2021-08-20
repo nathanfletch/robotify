21eb9ccb102f9bfa65588fa5b383014f248817c5
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-20 11:01:54 -0700

    Modify robotify() to replace numbers with 2 with Boop with priority over replacing 1 with Beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 function robotify(numString) {
-  let num = numString.trimStart().trim()
-  if (!/^\+?\d+$/.test(num))
-    return ["I only understand a single integer"];
+  let num = numString.trimStart().trim();
+  if (!/^\+?\d+$/.test(num)) return ["I only understand a single integer"];
   num = parseInt(num);
 
   let message = [];
-  for(let i = 0; i <= num; i++) {
-    if(i.toString().includes("1")) {
+  for (let i = 0; i <= num; i++) {
+    if (i.toString().includes("2")) {
+      message.push("Boop!");
+    } else if (i.toString().includes("1")) {
       message.push("Beep!");
     } else {
       message.push(i.toString());
